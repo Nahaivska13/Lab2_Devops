b97eeb0eaecd6176b61d48b7a0e0f13ed5f12f18
Nahaivska13 <Nahaivska13.daryna.nahaivska2017@gmail.com>
2024-10-26 18:47:25 +0000

    Hotfix: calculate only the first 3 terms of sec(x)

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,8 +1,12 @@
 // FuncA.cpp
 #include "FuncA.h"
-
-
-double FuncAClass::FuncA() {
-    return 0.0;
-}
-
+#include <cmath>
+   double FuncA::calculate(double x) {
+       double result = 0.0;
+       for (int k = 0; k < 3; ++k) {  // Only first 3 elements
+           double term = pow(-1, k) * eulerNumbers[k] * pow(x, 2 * k) / tgamma(2 * k + 1);
+           result += term;
+       }
+       return result;
+  }
+   
